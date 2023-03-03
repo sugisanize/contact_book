d6100b1f0b33ec994fb7f130b6e69c6e318f95cc
sugisanize <sugitaku0818@yahoo.co.jp>
2023-03-04 00:09:53 +0900

    Userモデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    child_first_name      {'じろう'}
+    team_id               {'2'}
+    email                 {'test@example'}
+    password              {'test1111'}
+    password_confirmation {password}
+    last_name             {'山田'}
+    first_name            {'太郎'}
+    last_name_kana        {'ヤマダ'}
+    first_name_kana       {'タロウ'}
+  end
+end
