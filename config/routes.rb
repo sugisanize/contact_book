8edab0ef4fd3c3f84105e994000b0f05bf47f317
sugisanize <sugitaku0818@yahoo.co.jp>
2023-03-04 11:37:06 +0900

    権限管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers:{
+    registrations: 'admins/registrations',
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    confirmations: 'admins/confirmations'
+  }
+  devise_for :users, controllers:{
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
   get 'contacts/index'
-  devise_for :users
   root to: "contacts#index"
   resources :users, only: [:edit, :update]
   resources :contacts do
