74a386ae68f15bab0269a0e87890df798ca6f793
sugisanize <sugitaku0818@yahoo.co.jp>
2023-04-27 22:23:24 +0900

    ユーザー一覧ページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @users = User.all
+    @users = User.where(team_id: params[:id])
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'contacts/index'
   root to: "contacts#index"
-  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy]
+  get '/users/team/:id', to: 'users#index'
   get 'withdrawal/:id', to: 'users#withdrawal', as: 'withdrawal'
   resources :contacts do
     resources :comments, only: [:index, :new, :create, :edit, :update, :destroy]
