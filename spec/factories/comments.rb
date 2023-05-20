8ecd4ec0a0835a33a6140edee40bf973ab6ae82c
sugisanize <sugitaku0818@yahoo.co.jp>
2023-05-20 23:47:15 +0900

    コメント単体テストコードにおけるインスタンス設定

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :comment do
+    sleepingstarttime    { '12:00' }
+    sleepingendtime      { '14:00' }
+    defecationtime       { '16:00' }
+    defecationquality_id { '2' }
+    mood_id              { '2' }
+    lunchamount_id       { '2' }
+    snackamount_id       { '2' }
+    comment_text         { '一日元気いっぱい遊びました。' }
+    association :contact
+    association :user
   end
 end
