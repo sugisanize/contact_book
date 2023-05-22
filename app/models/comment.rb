805d2791f0fd04cb1e29009a091a1e20370afc72
sugisanize <sugitaku0818@yahoo.co.jp>
2023-05-22 23:36:23 +0900

    コメントモデルのバリデーション修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,6 @@ class Comment < ApplicationRecord
                                 allow_blank: true
   validates :sleepingendtime, format: { with: /\A[0-9]{2}:[0-9]{2}\z/, message: 'is invalid. Include colon(:)' },
                               allow_blank: true
-  validates :defecationtime, format: { with: /\A[0-9]{2}\z/, message: 'is invalid. Include colon(:)' }, allow_blank: true
+  validates :defecationtime, format: { with: /\A[0-9]{2}\z/, message: 'is invalid.' }, allow_blank: true
   validates :comment_text, length: { maximum: 500 }
 end
