d5e0a8bf7f671a181f264c75c452cc29978e6ceb
sugisanize <sugitaku0818@yahoo.co.jp>
2023-05-28 00:15:26 +0900

    コメント用のユーザーに紐づく連絡一覧表示の任意ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   get '/users/team/:id', to: 'users#index'
   get 'withdrawal/:id', to: 'users#withdrawal', as: 'withdrawal'
   resources :contacts do
-    resources :comments, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
+  get 'users/:id/contacts', to: 'comments#index', as: 'comments'
 
 end
