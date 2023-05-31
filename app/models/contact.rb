c0e4fd16e9a37fa24e93cdd197c8255e8139857a
sugisanize <sugitaku0818@yahoo.co.jp>
2023-05-31 22:01:35 +0900

    contactモデルのpicuppersonバリデーション設定

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -20,5 +20,6 @@ class Contact < ApplicationRecord
   validates :dinnertime, format: { with: /\A[0-9]{2}:[0-9]{2}\z/, message: 'is invalid. Include colon(:)' }, allow_blank: true
   validates :breakfasttime, format: { with: /\A[0-9]{2}:[0-9]{2}\z/, message: 'is invalid. Include colon(:)' }, allow_blank: true
   validates :picuptime, format: { with: /\A[0-9]{2}:[0-9]{2}\z/, message: 'is invalid. Include colon(:)' }, allow_blank: true
+  validates :picupperson, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true
   validates :contact_text, length: { maximum: 500 }
 end
