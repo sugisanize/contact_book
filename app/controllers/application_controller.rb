63b7769168e2071f63e0fd4b86096d9a9d2521c1
sugisanize <sugitaku0818@yahoo.co.jp>
2023-06-11 20:38:35 +0900

    devise_parameter_sanitizerメソッド修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:child_first_name, :team_id, :last_name, :first_name, :last_name_kana,
-                                             :first_name_kana])
+                                             :first_name_kana, :telephonenumber])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:child_first_name, :team_id, :last_name, :first_name, :last_name_kana, :first_name_kana, :telephonenumber])
   end
 
   def basic_auth
