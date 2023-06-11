853416b2740c2c0e2e82e6b3e697b358e2799615
sugisanize <sugitaku0818@yahoo.co.jp>
2023-06-11 21:23:29 +0900

    usersコントローラーのuser_paramsにtelephonenumber追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:child_first_name, :team_id, :email, :last_name, :first_name, :last_name_kana, :first_name_kana)
+    params.require(:user).permit(:child_first_name, :team_id, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :telephonenumber)
   end
 end
